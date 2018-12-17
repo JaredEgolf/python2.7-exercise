103c26008eace232e0926eef379d529691dc4405
Jared Egolf <jaglor@pm.me>
2018-12-17 07:36:28 -0700

    BMI functions

---
+++ BMI-functions.py
@@ -0,0 +1,13 @@
+#!/bin/python
+
+height = float(raw_input("What is your height? (inches or meters) "))
+weight = float(raw_input("What is your weight? (pounds or kilos) "))
+unit = raw_input("Are your measurements in metric or imperial? ").lower().strip()
+
+if unit.startswith('i'):
+    bmi = 703 * (weight / (height ** 2))
+elif unit.startswith('m'):
+    bmi = (weight / (height ** 2))
+
+print("Your BMI is %s" % bmi)
+
