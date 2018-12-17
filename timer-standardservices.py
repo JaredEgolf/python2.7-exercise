4c4ca17870140abcfe26a9aa8493a5cff0255539
Jared Egolf <jaglor@pm.me>
2018-12-17 08:38:15 -0700

    timer / standardservices

---
+++ timer-standardservices.py
@@ -0,0 +1,16 @@
+#!/bin/python
+
+import time
+
+start_time = time.localtime()
+print("Timer started at %s" % time.strftime("%X", start_time))
+
+# Wait for user input
+raw_input("Please press Enter to continue...")
+
+stop_time = time.localtime()
+difftime = time.mktime(stop_time) - time.mktime(start_time)
+
+print("Timer stopped at %s" %s time.strftime("%X", stop_time))
+print("Total time: %s seconds" % difftime)
+
