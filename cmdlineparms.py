572027cd88add3ea0f7ea130bb07ab9a23941f8b
Jared Egolf <jaglor@pm.me>
2018-12-17 13:26:26 -0700

    cmdlineparms and reverse

--- cmdlineparms.py
+++ cmdlineparms.py
@@ -1,9 +1,14 @@
 #!/bin/python
 
 # Simplified parameters
-import sys
+# import sys
 
-print("First argument %s" % sys.argv[1])
+# print("First argument %s" % sys.argv[1])
 
 # Robust Method
+import argparse
+
+parser = argparse.ArgumentParser() # Class 'ArgumentParser' with a constructor function
+parser.add_argument('filename', help='The helpfile') # Method of the class
+args = parser.parse_args()
 
---
+++ reversefile.py
@@ -0,0 +1,28 @@
+#!/bin/python
+
+# Simplified parameters
+# import sys
+
+# print("First argument %s" % sys.argv[1])
+
+# Robust Method
+import argparse
+
+parser = argparse.ArgumentParser(description='Read a file in reverse') # Class 'ArgumentParser' with a constructor function
+parser.add_argument('filename', help='The helpfile') # Method of the class
+parser.add_argument('--limit', '-l', type=int, help='The number of lines to read')
+parser.add_argument('--version', '-v', action='version', version='%(prog)s 1.0')
+
+args = parser.parse_args()
+
+#print(args.filename)
+
+with open(args.filename) as f:
+    lines = f.readlines()
+    lines.reverse()
+
+    if args.limit:
+        lines = lines[:args.limit]
+
+    for line in lines:
+        print(line.strip()[::-1])
