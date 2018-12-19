62f66d9d67583e222145f5a8ec027b78aea340b0
Jared Egolf <jaglor@pm.me>
2018-12-19 07:12:03 -0700

    receipts upgraded

--- process_receipts.py
+++ process_receipts.py
@@ -4,6 +4,8 @@ import glob
 import os
 import shutil
 import json
+import re # Regular Expression Package
+import math
 
 try:
     os.mkdir("./processed")
@@ -11,7 +13,11 @@ except OSError:
     print("'processed' directory already exists")
 
 # Get a list of receipts
-receipts = glob.glob('./new/receipt-[0-9]*.json')
+# receipts = glob.glob('./new/receipt-[0-9]*.json')
+
+# Get only even number receipts
+receipts = [f for f in glob.iglob('./new/receipt-[0-9]*.json')
+        if re.match('./new/receipt-[0-9]*[02468].json', f)]
 
 subtotal = 0.0
 
@@ -19,12 +25,11 @@ for path in receipts:
     with open(path) as f:
         content = json.load(f)
         subtotal += float(content['value'])
-    name = path.split('/')[-1]
-    destination = "./processed/%s" % name
+    destination = path.replace('new', 'processed')
     shutil.move(path, destination)
     print("moved '%s' to '%s'" % (path, destination))
 
-print("Receipt subtotal: $%.2f" % subtotal)
+print("Receipt subtotal: $%s" % round(subtotal,2))
 
 # Iterate over the receipts
     # read content and tally a subtotal
